614956a8074cf6834ff4aa1ea0a45ac9daa9ddeb
Maxim Tyufyakov <maximtyufyakov@gmail.com>
2025-07-06 23:25:15 +0300

    Comments for try host db.

--- lib/common/models/db.model.dart
+++ lib/common/models/db.model.dart
@@ -5,10 +5,12 @@ class DbModel {
   /// The geosurveys database.
   static final PostgresDb geosurveysDb = PostgresDb(
     host: '10.0.2.2',
+    // host: '192.168.237.180',
+    // port: 5432,
     databaseName: 'geosurveys',
     username: 'postgres',
     password: 'admin',
-    queryTimeoutInSeconds: 5,
-    timeoutInSeconds: 5,
+    // queryTimeoutInSeconds: 5,
+    // timeoutInSeconds: 5,
   );
 }
