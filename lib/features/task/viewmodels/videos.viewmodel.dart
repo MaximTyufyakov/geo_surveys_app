2fe46efef297afa0f9b823e7f2b4544341c56857
Maxim Tyufyakov <maximtyufyakov@gmail.com>
2025-12-01 23:03:31 +0300

    Check empty and unique video title.

--- lib/features/task/viewmodels/videos.viewmodel.dart
+++ lib/features/task/viewmodels/videos.viewmodel.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -26,36 +27,62 @@ class VideosViewModel extends ChangeNotifier {
 
   /// Open page with camera and add new video in the list.
   void videoCreate(BuildContext context) async {
-    /// The title does not exist.
-    if (newTitleController.text == '') {
+    // Without front and back spaces.
+    String title = newTitleController.text.trim();
+
+    // The title does not exist.
+    if (title == '') {
       await showDialog<bool>(
         context: context,
         builder: (context) => AbstractDialog(
           title: 'Ошибка',
-          content: const Text('Название видео не введено.'),
+          content: const Text('Название видео пустое.'),
           greenTitle: 'Ок',
           redTitle: null,
         ),
       );
       return;
 
-      /// The title exists.
+      // The title does not unique.
+    } else if (!uniqueTitle(title)) {
+      await showDialog<bool>(
+        context: context,
+        builder: (context) => AbstractDialog(
+          title: 'Ошибка',
+          content: const Text('Название видео не уникально.'),
+          greenTitle: 'Ок',
+          redTitle: null,
+        ),
+      );
+      return;
+
+      // The title exist and unique.
     } else {
       final File? videoFile = await Navigator.pushNamed(
         context,
         '/video_shoot',
       ) as File?;
 
-      /// The video returned.
+      // The video returned.
       if (videoFile != null) {
         final VideoModel video = VideoModel(
           videoid: null,
-          title: newTitleController.text,
+          title: title,
           file: videoFile,
         );
         model.addVideo(video);
         notifyListeners();
       }
     }
+  }
+
+  /// Check unique video title.
+  ///
+  /// Param [title] is new title.
+  /// Returns true if unique.
+  bool uniqueTitle(String title) {
+    HashSet<String> hashSet = HashSet<String>()
+      ..addAll(model.videos.map((video) => video.title).toSet());
+    return !hashSet.contains(title);
   }
 }
